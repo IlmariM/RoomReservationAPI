832f10ab069d0d5b855be1acdeba113446a93bb2
Ilmari Metsävainio <ilmevavi@gmail.com>
2026-01-27 18:34:55 +0200

    refactorointi: muutin DbContextin käyttämään ensisijaista constructoria

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,10 +3,8 @@ using RoomReservationAPI.Models;
 
 namespace RoomReservationAPI.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-
         public DbSet<RoomReservation> RoomReservations { get; set; }
     }
 }
