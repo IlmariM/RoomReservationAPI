9d4e18af8ed6f456d2498823ebbdff3f6ff9ee9a
Ilmari Metsävainio <ilmevavi@gmail.com>
2026-01-27 18:03:49 +0200

    rafactorointi: lisäsin data annotaatiot modeliin

--- Models/RoomReservation.cs
+++ Models/RoomReservation.cs
@@ -1,12 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RoomReservationAPI.Models
 {
     public class RoomReservation
     {
         public int Id { get; set; }
-        public int RoomNumber { get; set; } // Range 1-5, can add validation later
+        [Range(1, 5, ErrorMessage = "RoomNumber must be between 1 and 5")]
+        public int RoomNumber { get; set; }
+
+        [Required(ErrorMessage = "Reservation start time is required")]
         public DateTime ReservationStart { get; set; }
+        [Required(ErrorMessage = "Reservation end time is required")]
         public DateTime ReservationEnd { get; set; }
+
+        [Required(ErrorMessage = "ReserverName is required")]
+        [StringLength(100)]
         public string? ReserverName { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string TimeZoneId { get; set; } = "UTC"; // IANA timezone identifier (e.g., "Europe/Helsinki", "America/New_York")
     }
 }
